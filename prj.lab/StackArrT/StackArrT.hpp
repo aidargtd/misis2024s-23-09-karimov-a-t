b81b6a471769792d3a044f68e300e44f4dfd0d63
aidargtd <karimovgtd@yandex.ru>
2024-05-20 18:58:38 +0300

    edit StackArrT

--- prj.lab/StackArrT/StackArrT.hpp
+++ prj.lab/StackArrT/StackArrT.hpp
@@ -1,123 +1,200 @@
-#include <initializer_list>
-#include <stdexcept>
-#include <algorithm>
+#include "iostream"
 
-template <typename T>
+template<typename T>
 class StackArrT {
 public:
-    StackArrT() : size_(0), i_top_(-1), data_(nullptr) {}
+    StackArrT();
 
-    ~StackArrT() {
-        delete[] data_;
-    }
+    ~StackArrT();
 
-    StackArrT(const StackArrT<T>& other) : size_(other.size_), i_top_(other.i_top_), data_(new T[other.size_]) {
-        std::copy(other.data_, other.data_ + other.size_, data_);
-    }
+    StackArrT(const StackArrT<T> &other);
 
-    StackArrT(StackArrT<T>&& other) noexcept : size_(other.size_), i_top_(other.i_top_), data_(other.data_) {
-        other.size_ = 0;
-        other.i_top_ = -1;
-        other.data_ = nullptr;
-    }
+    StackArrT(StackArrT<T> &&other);
 
-    StackArrT(const std::initializer_list<T>& list) : size_(list.size()), i_top_(list.size() - 1), data_(new T[list.size()]) {
-        std::copy(list.begin(), list.end(), data_);
-    }
+    StackArrT(const std::initializer_list<T> &list);
 
-    void push(const T& value) {
-        if (i_top_ + 1 >= size_) resize(size_ * 2 + 1);
-        data_[++i_top_] = value;
-    }
+    void push(const T &value);
 
-    void pop() {
-        if (empty()) throw std::underflow_error("Stack is empty");
-        --i_top_;
-    }
+    void pop();
 
-    T& top() const {
-        if (empty()) throw std::underflow_error("Stack is empty");
-        return data_[i_top_];
-    }
+    T &top() const;
 
-    void swap(StackArrT<T>& other) {
-        std::swap(size_, other.size_);
-        std::swap(i_top_, other.i_top_);
-        std::swap(data_, other.data_);
-    }
+    void swap(StackArrT<T> &other);
 
-    void merge(StackArrT<T>& other) {
-        if (other.empty()) return;
-        std::ptrdiff_t newSize = size_ + other.size_;
-        T* newData = new T[newSize];
-        std::copy(data_, data_ + size_, newData);
-        std::copy(other.data_, other.data_ + other.size_, newData + size_);
-        delete[] data_;
-        data_ = newData;
-        size_ = newSize;
-        i_top_ = size_ - 1;
-        other.clear(); // Clear other stack
-    }
+    void merge(StackArrT<T> &other);
 
-    [[nodiscard]] bool empty() const {
-        return size_ == 0;
-    }
+    bool empty() const;
 
-    [[nodiscard]] std::ptrdiff_t size() const {
-        return i_top_ + 1;
-    }
+    std::ptrdiff_t size() const;
 
-    void clear() {
-        delete[] data_;
-        size_ = 0;
-        i_top_ = -1;
-        data_ = nullptr;
-    }
+    bool operator==(const StackArrT<T> &rhs) const;
 
-    bool operator==(const StackArrT<T>& rhs) const {
-        if (size() != rhs.size()) return false;
-        for (std::ptrdiff_t i = 0; i <= i_top_; ++i) {
-            if (data_[i] != rhs.data_[i]) return false;
-        }
-        return true;
-    }
+    bool operator!=(const StackArrT<T> &rhs) const;
 
-    bool operator!=(const StackArrT<T>& rhs) const {
-        return !(*this == rhs);
-    }
+    StackArrT<T> &operator=(const StackArrT<T> &rhs) noexcept;
 
-    StackArrT<T>& operator=(const StackArrT<T>& rhs) noexcept {
-        if (this != &rhs) {
-            StackArrT<T> temp(rhs);
-            swap(temp);
-        }
-        return *this;
-    }
-
-    StackArrT<T>& operator=(StackArrT<T>&& other) noexcept {
-        if (this != &other) {
-            delete[] data_;
-            data_ = other.data_;
-            size_ = other.size_;
-            i_top_ = other.i_top_;
-            other.data_ = nullptr;
-            other.size_ = 0;
-            other.i_top_ = -1;
-        }
-        return *this;
-    }
+    StackArrT<T> &operator=(StackArrT<T> &&other);
 
 private:
-    std::ptrdiff_t size_;
-    std::ptrdiff_t i_top_;
-    T* data_;
-
-    void resize(std::ptrdiff_t newSize) {
-        if (newSize <= size_) return;
-        T* newData = new T[newSize];
-        std::copy(data_, data_ + size_, newData);
-        delete[] data_;
-        data_ = newData;
-        size_ = newSize;
-    }
+    std::ptrdiff_t size_ = 0;   //!< число элементов в буфере
+    std::ptrdiff_t i_top_ = -1; //!< индекс top элемента
+    T *data_ = nullptr;   //!< элементы стека
 };
+
+
+template<typename T>
+StackArrT<T>::StackArrT() = default;
+
+template<typename T>
+StackArrT<T>::~StackArrT() {
+    size_ = 0;
+    i_top_ = -1;
+    delete[] data_;
+    data_ = nullptr;
+}
+
+template<typename T>
+StackArrT<T>::StackArrT(const StackArrT<T> &other) {
+    data_ = new T[other.size_];
+    i_top_ = other.i_top_;
+    size_ = other.size_;
+    std::copy(other.data_, other.data_ + other.size_, data_);
+
+}
+
+template<typename T>
+StackArrT<T>::StackArrT(StackArrT<T> &&other) {
+    data_ = other.data_;
+    i_top_ = other.i_top_;
+    size_ = other.size_;
+
+    other.data_ = nullptr;
+    other.i_top_ = -1;
+    other.size_ = 0;
+    // StackArrT<T>::swap(*this, other);
+
+}
+
+template<typename T>
+StackArrT<T>::StackArrT(const std::initializer_list<T> &list) {
+    size_ = list.size();
+    i_top_ = size_ - 1;
+    data_ = new T[size_];
+    std::copy(list.begin(), list.end(), data_);
+}
+
+template<typename T>
+void StackArrT<T>::push(const T &value) {
+    if (data_ == nullptr) { // если стэк еще не создан, создаем
+        size_ = 8;
+        data_ = new T[size_];
+    } else if (size_ == i_top_ + 1) { // если есть стек, но в нем закончилось место для элемента. умножаем сайз на 2
+        T *new_data_ = new T[size_ * 2];
+        std::copy(data_, data_ + size_, new_data_);
+        size_ *= 2;
+        std::swap(new_data_, data_);
+        delete[] new_data_;
+    }
+    ++i_top_;
+
+    data_[i_top_] = value; // если место есть в стеке, то закидываем просто элемент
+
+}
+
+
+template<typename T>
+void StackArrT<T>::pop() {
+    if (empty()) {
+        throw std::out_of_range("Stack is empty :(((");
+    } else {
+        i_top_--;
+    }
+}
+
+template<typename T>
+T &StackArrT<T>::top() const {
+    if (empty()) {
+        throw std::out_of_range("Stack is empty :(((");
+    } else {
+        return data_[i_top_];
+
+    }
+}
+
+template<typename T>
+void StackArrT<T>::swap(StackArrT<T> &other) {
+    std::swap(other.data_, data_);
+    std::swap(other.size_, size_);
+    std::swap(other.i_top_, i_top_);
+}
+
+template<typename T>
+void StackArrT<T>::merge(StackArrT<T> &other) { // просто склейка двух стэков, без сортировки.
+    // если нужно с сотрировкой то сначал сортируем std::sort каждый стэк
+    if (empty()) {
+        this->swap(other);
+    }
+    else {
+        auto new_data_ = new T[size_ + other.size_];
+        std::copy(data_, data_ + this->size(), new_data_);
+        std::copy(other.data_, other.data_ + other.size(), new_data_ + this->size());
+        i_top_ += other.i_top_ + 1;
+        std::swap(new_data_, data_);
+        delete[] new_data_;
+        //delete [] other.data_;
+        size_ += other.size_;
+    }
+}
+
+template<typename T>
+bool StackArrT<T>::empty() const {
+    return (i_top_ == -1);
+}
+
+template<typename T>
+std::ptrdiff_t StackArrT<T>::size() const {
+    return i_top_ + 1;
+}
+
+template<typename T>
+StackArrT<T> &StackArrT<T>::operator=(const StackArrT<T> &rhs) noexcept {
+    data_ = new T[rhs.size_];
+    i_top_ = rhs.i_top_;
+    size_ = rhs.size_;
+    std::copy(rhs.data_, rhs.data_ + rhs.size_, data_);
+    return *this;
+}
+
+
+template<typename T>
+StackArrT<T> &StackArrT<T>::operator=(StackArrT<T> &&other) {
+    data_ = other.data_;
+    i_top_ = other.i_top_;
+    size_ = other.size_;
+
+    other.data_ = nullptr;
+    other.i_top_ = -1;
+    other.size_ = 0;
+    return *this;
+    // StackArrT<T>::swap(*this, other);
+
+}
+
+template<typename T>
+bool StackArrT<T>::operator==(const StackArrT<T> &rhs) const {
+    if (i_top_ != rhs.i_top_){
+        return false;
+    }
+    for (size_t i = 0; i < rhs.size(); ++i) {
+        if (data_[i] != rhs.data_[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+template<typename T>
+bool StackArrT<T>::operator!=(const StackArrT<T> &rhs) const {
+    return !(this->operator==(rhs));
+}
--- prj.test/StackArrT_test.cpp
+++ prj.test/StackArrT_test.cpp
@@ -1,59 +1,94 @@
-#include "StackArrT/StackArrT.hpp"
 #include <cassert>
+#include <iostream>
+#include "StackArrT/StackArrT.hpp"
 
-void test_stack_operations() {
-    // Создание пустого стека
+// Тест конструктора по умолчанию
+void testDefaultConstructor() {
     StackArrT<int> stack;
-    assert(stack.empty());
-    assert(stack.size() == 0);
+    assert(stack.empty() && "Default constructor test failed: stack should be empty");
+    std::cout << "Default constructor test passed!" << std::endl;
+}
 
-    // Проверка на пустоту стека перед удалением элементов
-    if (!stack.empty()) {
-        stack.pop();
-    }
-    assert(stack.empty());
-    assert(stack.size() == 0);
-
-    // Добавление элементов в стек
-    stack.push(5);
-    assert(!stack.empty());
-    assert(stack.size() == 1);
-    assert(stack.top() == 5);
-
-    stack.push(10);
-    assert(stack.size() == 2);
-    assert(stack.top() == 10);
-
-    // Удаление элементов из стека
-    if (!stack.empty()) {
-        stack.pop();
-    }
-    assert(stack.size() == 1);
-    assert(stack.top() == 5);
-
-    if (!stack.empty()) {
-        stack.pop();
-    }
-    assert(stack.empty());
-    assert(stack.size() == 0);
-
-    // Повторное добавление элементов и проверка на равенство и неравенство стеков
+// Тест функции push и top
+void testPushAndTop() {
+    StackArrT<int> stack;
     stack.push(1);
     stack.push(2);
     stack.push(3);
-
-    StackArrT<int> other_stack = stack;
-    assert(stack == other_stack);
-
-    if (!other_stack.empty()) {
-        other_stack.pop();
-    }
-    assert(stack != other_stack);
+    assert(stack.top() == 3 && "Push and top test failed: top element should be 3");
+    std::cout << "Push and top test passed!" << std::endl;
 }
 
+// Тест функции pop
+void testPop() {
+    StackArrT<int> stack;
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.pop();
+    assert(stack.top() == 2 && "Pop test failed: top element should be 2 after pop");
+    stack.pop();
+    assert(stack.top() == 1 && "Pop test failed: top element should be 1 after second pop");
+    stack.pop();
+    assert(stack.empty() && "Pop test failed: stack should be empty after popping all elements");
+    std::cout << "Pop test passed!" << std::endl;
+}
 
+// Тест конструктора копирования
+void testCopyConstructor() {
+    StackArrT<int> stack1;
+    stack1.push(1);
+    stack1.push(2);
+    stack1.push(3);
+
+    StackArrT<int> stack2(stack1);
+    assert(stack2.top() == 3 && "Copy constructor test failed: top element should be 3");
+    stack2.pop();
+    assert(stack2.top() == 2 && "Copy constructor test failed: top element should be 2 after pop");
+    std::cout << "Copy constructor test passed!" << std::endl;
+}
+
+// Тест конструктора перемещения
+void testMoveConstructor() {
+    StackArrT<int> stack1;
+    stack1.push(1);
+    stack1.push(2);
+    stack1.push(3);
+
+    StackArrT<int> stack2(std::move(stack1));
+    assert(stack2.top() == 3 && "Move constructor test failed: top element should be 3");
+    stack2.pop();
+    assert(stack2.top() == 2 && "Move constructor test failed: top element should be 2 after pop");
+    assert(stack1.empty() && "Move constructor test failed: original stack should be empty");
+    std::cout << "Move constructor test passed!" << std::endl;
+}
+
+// Тест функции merge
+void testMerge() {
+    StackArrT<int> stack1;
+    stack1.push(1);
+    stack1.push(2);
+
+    StackArrT<int> stack2;
+    stack2.push(3);
+    stack2.push(4);
+
+    stack1.merge(stack2);
+    assert(stack1.size() == 4 && "Merge test failed: stack1 size should be 4 after merge");
+    assert(stack1.top() == 4 && "Merge test failed: top element should be 4 after merge");
+    stack1.pop();
+    assert(stack1.top() == 3 && "Merge test failed: top element should be 3 after pop");
+    std::cout << "Merge test passed!" << std::endl;
+}
 
 int main() {
-    test_stack_operations();
+    testDefaultConstructor();
+    testPushAndTop();
+    testPop();
+    testCopyConstructor();
+    testMoveConstructor();
+    testMerge();
+
+    std::cout << "All tests passed!" << std::endl;
     return 0;
 }
